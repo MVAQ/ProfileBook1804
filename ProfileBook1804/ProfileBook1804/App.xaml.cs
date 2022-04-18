abd650a95beb42034879be25fdaf393228bf6758
MVAQ <vladimirmasalv3@gmail.com>
2022-04-18 23:02:32 +0300

    Add project files.

---
+++ ProfileBook1804/ProfileBook1804/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism.Unity;
+using Prism.Navigation;
+using Prism.Ioc;
+using Prism;
+using ProfileBook1804.Views;
+
+namespace ProfileBook1804
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync($"{nameof(ProfileBook1804.Views.SingInView)}");
+        }
+  
+
+        protected override async void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<SingInView>();
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+
+    }
+}
